05780d25a5ff039451e5d8ede4fd11ee907dc243
4885226 <ivan.alputov@mail.ru>
2023-10-10 01:07:00 +0300

    stupid bitfield dont wont to complete a tests

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -124,6 +124,10 @@ TBitField& TBitField::operator=(const TBitField& bf) // присваивание
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
 {
+	//if (&bf == nullptr)
+	//{
+	//	throw "bf=null ptr";
+	//}
 	if (BitLen != bf.BitLen)
 	{
 		return false;
@@ -153,54 +157,219 @@ int TBitField::operator!=(const TBitField &bf) const // сравнение
 
 TBitField TBitField::operator|(const TBitField &bf) // операция "или"
 {
-	if (BitLen != bf.BitLen)
-	{
-		throw "bifield sizes are not equal ";
-	}
-	TBitField res(MemLen);
-	for (int i = 0; i < MemLen; i++)
-	{
-		res.pMem[i] = (pMem[i] | bf.pMem[i]);
-	}
-	/*for (int i = 0; i < BitLen; i++)
-	{
-		if ((GetBit(i) | bf.GetBit(i))!=0)
-	    {
-	       res.SetBit(i);
-	    }
+	//int MEMmin = 0;
+	//int MEMmax = 0;
+	//if (BitLen != bf.BitLen)
+	//{
+	//	int max = bf.BitLen;
+	//	int min = BitLen;
+	//	if (BitLen > bf.BitLen)
+	//	{
+	//		max = BitLen;
+	//		min = bf.BitLen;
+	//	}
+	//	if (min == BitLen)
+	//	{
+	//		MEMmin = MemLen;
+	//	}
+	//	else
+	//	{
+	//		MEMmin = bf.MemLen;
+	//	}
 
-	}*/
-	return res;//jncskrf
+	//	TBitField res(max);
+	//	if (max == BitLen)
+	//	{
+	//		for (int i = 0; i < MemLen; i++)
+	//		{
+	//			res.pMem[i] = pMem[i];
+	//		}
+	//		for (int i = 0; i < MEMmin - 2; i++)
+	//		{
+	//			res.pMem[i] = (bf.pMem[i] | res.pMem[i]);
+	//		}
+	//		for (int k = (MEMmin - 1) * 32; k < min; k++)
+	//		{
+	//			if ((bf.GetBit(k) | res.GetBit(k)) == 0)
+	//			{
+	//				res.ClrBit(k);
+	//			}
+	//			else
+	//			{
+	//				res.SetBit(k);
+	//			}
+
+
+	//		}
+	//		//TBitField help(bf);
+
+
+	//		TBitField help(bf);
+
+	//		for (int k = min; k < (MEMmin * 32) - 1; k++)
+	//		{
+
+
+	//			help.ClrBit(k);
+	//			if (res.GetBit(k) | help.GetBit(k) == 0)
+	//			{
+	//				res.ClrBit(k);
+	//			}
+	//			else
+	//			{
+	//				res.SetBit(k);
+	//			}
+
+	//		}
+
+	//	}
+	//	else
+	//	{
+	//		for (int i = 0; i < bf.MemLen; i++)
+	//		{
+	//			res.pMem[i] = bf.pMem[i];
+	//		}
+	//		for (int i = 0; i < MEMmin - 2; i++)
+	//		{
+	//			res.pMem[i] = (this->pMem[i] | res.pMem[i]);
+	//		}
+	//		for (int k = (MEMmin - 1) * 32; k < min; k++)
+	//		{
+	//			if ((this->GetBit(k) | res.GetBit(k)) == 0)
+	//			{
+	//				res.ClrBit(k);
+	//			}
+	//			else
+	//			{
+	//				res.SetBit(k);
+	//			}
+
+
+	//		}
+	//		//TBitField help(bf);
+
+
+
+
+	//		for (int k = min; k < (MEMmin * 32) - 1; k++)
+	//		{
+
+
+	//			this->ClrBit(k);
+	//			if (res.GetBit(k) | this->GetBit(k) == 0)
+	//			{
+	//				res.ClrBit(k);
+	//			}
+	//			else
+	//			{
+	//				res.SetBit(k);
+	//			}
+
+	//		}
+	//	}
+	//	return res;
+	//}
+	//else
+	//{
+	//	int maxl = BitLen;
+	//	TBitField res(maxl);
+	//	for (int i = 0; i < MemLen; i++)
+	//	{
+	//		res.pMem[i] = pMem[i];
+	//	}
+	//	for (int i = 0; i < bf.MemLen; i++)
+	//	{
+	//		res.pMem[i] = bf.pMem[i] | res.pMem[i];
+	//	}
+	//	return res;
+ //   }
+
+int min_len = bf.MemLen < this->MemLen ? bf.MemLen : this->MemLen;
+int min_bit_len = bf.BitLen < this->BitLen ? bf.BitLen : this->BitLen;
+int max_bit_len = bf.BitLen > this->BitLen ? bf.BitLen : this->BitLen;
+TBitField res = TBitField(max_bit_len);
+for (size_t i = 0; i < min_len; i++)
+{
+	res.pMem[i] = this->pMem[i] | bf.pMem[i];
+}
+return res;
 }
 
 TBitField TBitField::operator&(const TBitField &bf) // операция "и"
 {
-	if (BitLen != bf.BitLen)
-	{
-		throw "bifield sizes are not equal ";
-	}
-	TBitField res(BitLen);
-	for (int i = 0; i < MemLen; i++)
-	{
-		res.pMem[i] = (pMem[i] & bf.pMem[i]);
-	}
-	/*for (int i = 0; i < BitLen; i++)
-	{
-		if ((GetBit(i) & bf.GetBit(i))!=0)
-		{
-		   res.SetBit(i);
-		}
+	//int MEMmin = 0;
+	//int MEMmax = 0;
 
-	}*/
+	//int max = bf.BitLen;
+	//int min = BitLen;
+	//if (BitLen > bf.BitLen)
+	//{
+	//	max = BitLen;
+	//	min = bf.BitLen;
+	//}
+	//if (min == BitLen)
+	//{
+	//	MEMmin = MemLen;
+	//}
+	//else
+	//{
+	//	MEMmin = bf.MemLen;
+	//}
+
+	//TBitField res(max);
+	//for (int i = 0; i < MemLen; i++)
+	//{
+	//	res.pMem[i] = pMem[i];
+	//}
+	//for (int i = 0; i < MEMmin - 2; i++)
+	//{
+	//	res.pMem[i] = (bf.pMem[i] & res.pMem[i]);
+	//}
+	//for (int k = (MEMmin - 1) * 32; k < min; k++)
+	//{
+	//	if ((bf.GetBit(k) & res.GetBit(k)) == 0)
+	//	{
+	//		res.ClrBit(k);
+	//	}
+	//	else
+	//	{
+	//		res.SetBit(k);
+	//	}
+
+
+	//}
+	//TBitField help(bf);
+	//for (int k = min; k < (MEMmin * 32) - 1; k++)
+	//{
+	//	help.SetBit(k);
+	//	if (res.GetBit(k) & help.GetBit(k) == 0)
+	//	{
+	//		res.ClrBit(k);
+	//	}
+	//	else
+	//	{
+	//		res.SetBit(k);
+	//	}
+	//}
+
+	//return res
+	int min_len = bf.MemLen < this->MemLen ? bf.MemLen : this->MemLen;
+	int min_bit_len = bf.BitLen < this->BitLen ? bf.BitLen : this->BitLen;
+	int max_bit_len = bf.BitLen > this->BitLen ? bf.BitLen : this->BitLen;
+	TBitField res = TBitField(max_bit_len);
+	for (int i = 0; i < min_len; i++)
+	{
+		res.pMem[i] = this->pMem[i] & bf.pMem[i];
+	}
 	return res;
 }
 
 TBitField TBitField::operator~(void) // отрицание
 {
-	TBitField res(MemLen);
+	TBitField res(BitLen);
 	for (int i = 0; i < MemLen; i++)
 	{
-		res.pMem[i] = !(pMem[i]);
+		res.pMem[i] = ~pMem[i];
 	}
 	/*for (int i = 0; i < BitLen; i++)
 {
@@ -223,10 +392,30 @@ TBitField TBitField::operator~(void) // отрицание
 
  istream &operator>>(istream &istr, TBitField &bf) // ввод
 {
+	 char a;
+	 for (int i = 0; i < bf.MemLen; i++)
+	 {
+		 bf.pMem[i] = 0;
+	 }
 	for (int i = 0; i < bf.BitLen; i++)
 	{
 
-		cin >> bf.pMem[i];
+		istr >> a;
+		if (a == '1')
+		{
+			bf.SetBit(i);
+		}
+		else
+		{
+			if (a == '0')
+			{
+				bf.ClrBit(i);
+			}
+			else
+			{
+				break;
+			}
+		}
 	}
 	return istr;
 
@@ -234,10 +423,19 @@ TBitField TBitField::operator~(void) // отрицание
 
   ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
-	for (int i = 0; i < bf.MemLen; i++)
+	for (int i = 0; i < bf.BitLen; i++)
 	{
 
-		cout << bf.pMem[i];
+		if (bf.GetBit(i))
+		{
+			ostr << '1';
+
+		}
+		else
+		{
+			ostr << '0';
+		}
+
 	}
 	return ostr;
 
